294d6c93376f5d4d2a821e14ce495d3931318a31
dawidzabrzenski <dawidzabrzenski00@gmail.com>
2025-05-07 11:02:52 +0200

    feat: theme toggler

---
+++ frontend/src/components/ThemeToggle.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { Moon, Sun } from "lucide-react";
+
+const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (storedTheme === "dark" || (!storedTheme && prefersDark)) {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-2 rounded transition-colors "
+      aria-label="Toggle Dark Mode"
+    >
+      {isDarkMode ? <Sun size={24} /> : <Moon size={24} />}
+    </button>
+  );
+};
+
+export default ThemeToggle;
