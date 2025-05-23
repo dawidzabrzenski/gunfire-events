e45e1a8b97dff62b029726f26024272a64994442
dawidzabrzenski <dawidzabrzenski00@gmail.com>
2025-05-23 12:36:37 +0200

    chore: util for voivodeships

---
+++ frontend/src/utils/VoivodeshipsOptions.jsx
@@ -0,0 +1,24 @@
+function VoivodeshipsOptions() {
+  return (
+    <>
+      <option value="1">Dolnośląskie</option>
+      <option value="2">Kujawsko-Pomorskie</option>
+      <option value="3">Lubelskie</option>
+      <option value="4">Lubuskie</option>
+      <option value="5">Łódzkie</option>
+      <option value="6">Małopolskie</option>
+      <option value="7">Mazowieckie</option>
+      <option value="8">Opolskie</option>
+      <option value="9">Podkarpackie</option>
+      <option value="10">Podlaskie</option>
+      <option value="11">Pomorskie</option>
+      <option value="12">Śląskie</option>
+      <option value="13">Świętokrzyskie</option>
+      <option value="14">Warmińsko-Mazurskie</option>
+      <option value="15">Wielkopolskie</option>
+      <option value="16">Zachodniopomorskie</option>
+    </>
+  );
+}
+
+export default VoivodeshipsOptions;
